3cfc3d628334ac42513d4c5e89d007eb17cfa924
Данил Бухаров <danilvbuh@gmail.com>
2020-06-15 02:00:34 +0700

    Added Movie and User db models

---
+++ MovieCatalog/Migrations/20200614185707_AddMovies.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieCatalog.Migrations
+{
+    public partial class AddMovies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    NormalizedUserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatorId = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(maxLength: 200, nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Release = table.Column<int>(nullable: false),
+                    Producer = table.Column<string>(nullable: false),
+                    Poster = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_ApplicationUser_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "ApplicationUser",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CreatorId",
+                table: "Movies",
+                column: "CreatorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUser");
+        }
+    }
+}
---
+++ MovieCatalog/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCatalog.Models
+{
+	public class Movie
+	{
+		public Guid Id { get; set; } = Guid.NewGuid();
+
+		[Required]
+		public String CreatorId { get; set; }
+
+		public ApplicationUser Creator { get; set; }
+
+		public DateTime Created { get; set; }
+
+		public DateTime Modified { get; set; }
+
+		[Required]
+		[MaxLength(200)]
+		public String Title { get; set; }
+
+		[Required]
+		public String Description { get; set; }
+
+		[Required]
+		public int Release { get; set; }
+
+		[Required]
+		public String Producer { get; set; }
+
+		[Required]
+		public String Poster { get; set; }
+	}
+}
